3bec3f2fd4b425e39807ce2b7722f906c22222e6
Victor <username96543>
2023-09-08 10:49:28 +0300

    blog_cat

--- src/js/common/sliders.js
+++ src/js/common/sliders.js
@@ -1,34 +1,34 @@
 export function initSliders() {
-  const sliderDoctors = new Swiper('.doctors__slider', {
+  const sliderDoctors = new Swiper(".doctors__slider", {
     breakpoints: {
       320: {
         slidesPerView: 1.1,
-        spaceBetween: 16
+        spaceBetween: 16,
       },
       540: {
         slidesPerView: 2,
-        spaceBetween: 40
+        spaceBetween: 40,
       },
       991: {
         slidesPerView: 3,
-        spaceBetween: 40
+        spaceBetween: 40,
       },
       1280: {
         slidesPerView: 4,
-        spaceBetween: 40
-      }
+        spaceBetween: 40,
+      },
     },
     navigation: {
-      nextEl: '.doctors .swiper-button-next',
-      prevEl: '.doctors .swiper-button-prev',
+      nextEl: ".doctors .swiper-button-next",
+      prevEl: ".doctors .swiper-button-prev",
     },
   });
 
-  const sliderScreen9 = new Swiper('.screen-9__slider', {
+  const sliderScreen9 = new Swiper(".screen-9__slider", {
     breakpoints: {
       320: {
         slidesPerView: 1,
-        spaceBetween: 8
+        spaceBetween: 8,
       },
       540: {
         slidesPerView: 1.5,
@@ -36,63 +36,89 @@ export function initSliders() {
       },
       767: {
         slidesPerView: 2,
-        spaceBetween: 24
+        spaceBetween: 24,
       },
       991: {
         slidesPerView: 3,
-        spaceBetween: 24
-      }
+        spaceBetween: 24,
+      },
     },
     navigation: {
-      nextEl: '.screen-9 .swiper-button-next',
-      prevEl: '.screen-9 .swiper-button-prev',
+      nextEl: ".screen-9 .swiper-button-next",
+      prevEl: ".screen-9 .swiper-button-prev",
     },
   });
 
-  const sliderLicenses = new Swiper('.licenses__slider', {
+  const sliderLicenses = new Swiper(".licenses__slider", {
     breakpoints: {
       320: {
         slidesPerView: 1,
-        spaceBetween: 0
+        spaceBetween: 0,
       },
       540: {
         slidesPerView: 2,
-        spaceBetween: 24
+        spaceBetween: 24,
       },
       767: {
         slidesPerView: 3,
-        spaceBetween: 24
+        spaceBetween: 24,
       },
       991: {
         slidesPerView: 4,
-        spaceBetween: 40
-      }
+        spaceBetween: 40,
+      },
     },
     navigation: {
-      nextEl: '.licenses .swiper-button-next',
-      prevEl: '.licenses .swiper-button-prev',
+      nextEl: ".licenses .swiper-button-next",
+      prevEl: ".licenses .swiper-button-prev",
     },
   });
 
-  const sliderReview = new Swiper('.review__slider', {
+  const sliderReview = new Swiper(".review__slider", {
     breakpoints: {
       320: {
         slidesPerView: 1,
-        spaceBetween: 8
+        spaceBetween: 8,
       },
       540: {
         slidesPerView: 2,
-        spaceBetween: 24
+        spaceBetween: 24,
       },
       1280: {
         slidesPerView: 3,
-        spaceBetween: 40
-      }
+        spaceBetween: 40,
+      },
     },
     navigation: {
-      nextEl: '.review .swiper-button-next',
-      prevEl: '.review .swiper-button-prev',
+      nextEl: ".review .swiper-button-next",
+      prevEl: ".review .swiper-button-prev",
     },
   });
 
+  const sliderACategories = new Swiper(".articles-categories__slider", {
+    slidesPerView: "auto",
+    // breakpoints: {
+    //   320: {
+    //     slidesPerView: 1.1,
+    //     spaceBetween: 16,
+    //   },
+    //   540: {
+    //     slidesPerView: 2,
+    //     spaceBetween: 40,
+    //   },
+    //   991: {
+    //     slidesPerView: 3,
+    //     spaceBetween: 40,
+    //   },
+    //   1280: {
+    //     slidesPerView: 4,
+    //     spaceBetween: 40,
+    //   },
+    // },
+    spaceBetween: 40,
+    navigation: {
+      nextEl: ".articles-categories .swiper-button-next",
+      prevEl: ".articles-categories .swiper-button-prev",
+    },
+  });
 }
